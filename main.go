b0d2b37261645b1c198dd061df87cdba663f8a15
tangmengqiu <1124090103@qq.com>
2019-09-22 18:29:17 +0800

    sepreate command

--- main.go
+++ main.go
@@ -8,38 +8,6 @@ import (
 	"fmt"
 )
 
-var runCommand = cli.Command{
-	Name: "run",
-	Usage: `Create a container with namespace and cgroups limit
-			mydocker run -ti [command]`,
-	Flags: []cli.Flag{
-		cli.BoolFlag{
-			Name:  "ti",
-			Usage: "enable tty",
-		},
-	},
-	Action: func(context *cli.Context) error {
-		if len(context.Args()) < 1 {
-			return fmt.Errorf("Missing container command")
-		}
-		cmd := context.Args().Get(0)
-		tty := context.Bool("ti")
-		Run(tty, cmd)
-		return nil
-	},
-}
-
-var initCommand = cli.Command{
-	Name:  "init",
-	Usage: "Init container process run user's process in container. Do not call it outside",
-	Action: func(context *cli.Context) error {
-		log.Infof("init come on")
-		cmd := context.Args().Get(0)
-		log.Infof("command %s", cmd)
-		err := container.RunContainerInitProcess(cmd, nil)
-		return err
-	},
-}
 
 
 const usage = `mydocker is a simple container runtime implementation.
@@ -71,11 +39,4 @@ func main() {
 }
 
 
-func Run(tty bool, command string) {
-	parent := container.NewParentProcess(tty, command)
-	if err := parent.Start(); err != nil {
-		log.Error(err)
-	}
-	parent.Wait()
-	os.Exit(-1)
-}
+
