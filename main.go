c1ac5b032ac912afdcaf5ca8ac5342957e23b791
tangmengqiu <1124090103@qq.com>
2019-09-22 10:17:29 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+	"mydocker/container"
+	"os"
+	"fmt"
+)
+
+var runCommand = cli.Command{
+	Name: "run",
+	Usage: `Create a container with namespace and cgroups limit
+			mydocker run -ti [command]`,
+	Flags: []cli.Flag{
+		cli.BoolFlag{
+			Name:  "ti",
+			Usage: "enable tty",
+		},
+	},
+	Action: func(context *cli.Context) error {
+		if len(context.Args()) < 1 {
+			return fmt.Errorf("Missing container command")
+		}
+		cmd := context.Args().Get(0)
+		tty := context.Bool("ti")
+		Run(tty, cmd)
+		return nil
+	},
+}
+
+var initCommand = cli.Command{
+	Name:  "init",
+	Usage: "Init container process run user's process in container. Do not call it outside",
+	Action: func(context *cli.Context) error {
+		log.Infof("init come on")
+		cmd := context.Args().Get(0)
+		log.Infof("command %s", cmd)
+		err := container.RunContainerInitProcess(cmd, nil)
+		return err
+	},
+}
+
+
+const usage = `mydocker is a simple container runtime implementation.
+			   The purpose of this project is to learn how docker works and how to write a docker by ourselves
+			   Enjoy it, just for fun.`
+
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "mydocker"
+	app.Usage = usage
+
+	app.Commands = []cli.Command{
+		initCommand,
+		runCommand,
+	}
+
+	app.Before = func(context *cli.Context) error {
+		// Log as JSON instead of the default ASCII formatter.
+		log.SetFormatter(&log.JSONFormatter{})
+
+		log.SetOutput(os.Stdout)
+		return nil
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+func Run(tty bool, command string) {
+	parent := container.NewParentProcess(tty, command)
+	if err := parent.Start(); err != nil {
+		log.Error(err)
+	}
+	parent.Wait()
+	os.Exit(-1)
+}
